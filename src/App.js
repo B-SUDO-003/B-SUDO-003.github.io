3b9477f8be790177ef05b7e19171cfa19c81430f
Ajay-o-s <akhubsmaster@gmail.com>
2022-06-11 11:27:37 +0530

    added new

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Loader from "./components/Load";
+//import React, { useEffect, useState } from "react";
+//import Loader from "./components/Load";
 import Home from "./www/Home";
 
 
 function App() {
-	const [appLoaded, setAppLoaded] = useState(false);
-	const [startLoadProgress, setStartLoadProgress] = useState(false);
-
-	useEffect(() => {stopLoad();}, []);
-
-	const stopLoad = () => {
-		setStartLoadProgress(true);
-		setTimeout(() => setAppLoaded(true), 3000);
-	};
-
-	if (!appLoaded) return <Loader done={startLoadProgress} />;
+	//const [appLoaded, setAppLoaded] = useState(false);
+	//const [startLoadProgress, setStartLoadProgress] = useState(false);
+	//useEffect(() => {stopLoad();}, []);
+	//const stopLoad = () => {
+	//	setStartLoadProgress(true);
+	//	setTimeout(() => setAppLoaded(true), 3000);
+	//};
+	//if (!appLoaded) return <Loader done={startLoadProgress} />;
 
   return (
     <div className="App">
